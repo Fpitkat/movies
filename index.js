42688ee0cb4fa4b0d8bfaefc4209b67d96fb28c0
Frank Pitkat <fpitkat@gmail.com>
2023-02-13 20:28:30 -0500

    finish dropdown

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const API_KEY = '91483366'
 const url = `http://www.omdbapi.com/`
-const PosterAPI = 'http://img.omdbapi.com/?apikey=[yourkey]&'
+// const PosterAPI = 'http://img.omdbapi.com/?apikey=[yourkey]&'
 
 const input = document.querySelector('input')
 
@@ -8,12 +8,10 @@ const onInput = async (event) => {
   const movies = await fetchData(event.target.value)
   for (let movie of movies) {
     const div = document.createElement('div')
-
     div.innerHTML = `
-        <img src="${movie.Poster}" alt="${movie.Title}" />
+        <img src="${movie.Poster}" alt="image of ${movie.Title}" />
         <h1>${movie.Title}</h1>
       `
-
     const target = document.querySelector('#target')
     target.append(div)
   }
@@ -24,10 +22,12 @@ input.addEventListener('input', debounce(onInput, 500))
 const fetchData = async (searchTerm) => {
   const response = await axios.get('http://www.omdbapi.com/', {
     params: {
-      apikey: 'd9835cc5',
+      apikey: API_KEY,
       s: searchTerm,
     },
   })
-
+  if (response.data.Error) {
+    return []
+  }
   return response.data.Search
 }
