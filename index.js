e113c2862cf3ed0e455535301f128e641dc92299
Frank Pitkat <fpitkat@gmail.com>
2023-02-17 16:55:57 -0500

    finish section 245

--- index.js
+++ index.js
@@ -2,18 +2,52 @@ const API_KEY = '91483366'
 const url = `http://www.omdbapi.com/`
 // const PosterAPI = 'http://img.omdbapi.com/?apikey=[yourkey]&'
 
+const root = document.querySelector('.autocomplete')
+root.innerHTML = `
+  <label><strong>Search for Movie</strong></label>
+  <input class='input' />
+  <div class='dropdown'>
+    <div class='dropdown-menu'>
+      <div class='dropdown-content results'></div>
+    </div>
+  </div>
+`
+
 const input = document.querySelector('input')
+const dropdown = document.querySelector('.dropdown')
+const resultsWrapper = document.querySelector('.results')
+
+document.addEventListener('click', (event) => {
+  if (!root.contains(event.target)) {
+    dropdown.classList.remove('is-active')
+  }
+})
 
 const onInput = async (event) => {
   const movies = await fetchData(event.target.value)
+  if (!movies.length) {
+    dropdown.classList.remove('is-active')
+    return
+  }
+
+  resultsWrapper.innerHTML = ''
+  dropdown.classList.add('is-active')
+
   for (let movie of movies) {
-    const div = document.createElement('div')
-    div.innerHTML = `
-        <img src="${movie.Poster}" alt="image of ${movie.Title}" />
-        <h1>${movie.Title}</h1>
+    const option = document.createElement('a')
+    const imgSrc = movie.Poster === 'N/A' ? '' : movie.Poster
+
+    option.classList.add('dropdown-item')
+    option.innerHTML = `
+        <img src="${imgSrc}" alt="image of ${movie.Title}" />
+        ${movie.Title}
       `
-    const target = document.querySelector('#target')
-    target.append(div)
+    option.addEventListener('click', () => {
+      input.value = movie.Title
+      dropdown.classList.remove('is-active')
+      onMovieSelect(movie)
+    })
+    resultsWrapper.append(option)
   }
 }
 
@@ -30,4 +64,36 @@ const fetchData = async (searchTerm) => {
     return []
   }
   return response.data.Search
+}
+
+const onMovieSelect = async (movie) => {
+  const response = await axios.get('http://www.omdbapi.com/', {
+    params: {
+      apikey: API_KEY,
+      i: movie.imdbID,
+    },
+  })
+  console.log(response.data)
+  const summary = document.querySelector('#summary')
+  console.log(summary)
+  summary.innerHTML = movieTemplate(response.data)
+}
+
+const movieTemplate = (movieDetail) => {
+  return `
+    <article class='media'>
+      <figure class='media-left'>
+        <p class='image'>
+          <img src='${movieDetail.Poster}' />
+        </p>
+      </figure>
+      <div class='media-content'>
+        <div class='content'>
+          <h1>${movieDetail.Title}</h1>
+          <h4>${movieDetail.Genre}</h4>
+          <p>${movieDetail.Plot}</p>
+        </div>
+      </div>
+    </article>
+  `
 }
